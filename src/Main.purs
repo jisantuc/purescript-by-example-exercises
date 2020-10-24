a419fee6a3c0a193d21c0273193eb8e7a343b542
James Santucci <james.santucci@gmail.com>
2020-10-24 12:18:33 -0600

    Make main print Euler answer to console

--- src/Main.purs
+++ src/Main.purs
@@ -3,8 +3,9 @@ module Main where
 import Prelude
 
 import Effect (Effect)
+import Euler (answer)
 import Effect.Console (log)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log $ "The answer is " <> show answer
