405bec048923792f744bac448d037ee423ff00aa
James Santucci <james.santucci@gmail.com>
2020-10-24 12:40:32 -0600

    Add chapter 2 solutions

--- purescript-book/exercises/chapter2/test/MySolutions.purs
+++ purescript-book/exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,15 @@
 module Test.MySolutions where
 
+import Global (readFloat)
 import Prelude
+
+import Math (e, pi, sqrt)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea radius = pi * radius * radius
+
+addE :: String -> Number
+addE = (_ + e) <<< readFloat
